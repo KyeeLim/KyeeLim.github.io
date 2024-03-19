8afe14aa49c6eca1e32dc142eb5981385a4cbe9e
AmousuLee <leeamous@gmail.com>
2024-03-19 08:45:12 +0800

    Remember toggle grayscale

--- js/onClick.js
+++ js/onClick.js
@@ -1,10 +1,20 @@
-function onClick(){
-  // Add event listener to each image to toggle grayscale effect
-  document.querySelectorAll('img').forEach(img => {
-    img.addEventListener('click', () => {
-      img.classList.toggle('grayscale');
-    });
-  });
+function toggleGrayscale(img) {
+    img.classList.toggle('grayscale');
+    const imageName = img.alt;
+    const isGrayscale = img.classList.contains('grayscale');
+    localStorage.setItem(imageName, isGrayscale);
 }
 
-onClick();
+  // Add event listener to each image to toggle grayscale effect
+document.querySelectorAll('img').forEach(img => {
+  const imageName = img.alt;
+  const isGrayscale = localStorage.getItem(imageName) === 'true';
+  if (isGrayscale) {
+    img.classList.add('grayscale');
+  }
+
+  img.addEventListener('click', () => {
+    toggleGrayscale(img);
+  });
+});
+
