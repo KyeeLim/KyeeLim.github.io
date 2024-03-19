8a964ec8ebf469f8b4b179f84519ef9fcc442bd8
Kyee124 <lim.ky73@gmail.com>
2024-03-19 09:01:07 +0800

    Updated code

--- js/onClick.js
+++ js/onClick.js
@@ -1,6 +1,8 @@
 function onClick(){
   // Add event listener to each image to toggle grayscale effect
-  document.querySelectorAll('img').forEach(img => {
+  document.querySelectorAll('img:not(#banner)').forEach(img => {
+	//set grayscale to everything
+	img.classList.toggle('grayscale');
     img.addEventListener('click', () => {
       img.classList.toggle('grayscale');
     });
