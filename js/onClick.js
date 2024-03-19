b2269c3ab9a9ce437bca19bb3a1037f9c81e7f4d
Kyee124 <lim.ky73@gmail.com>
2024-03-19 09:10:10 +0800

    Update onClick.js

--- js/onClick.js
+++ js/onClick.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-function onClick(){
-  // Add event listener to each image to toggle grayscale effect
-  document.querySelectorAll('img:not(#banner)').forEach(img => {
-	//set grayscale to everything
-	img.classList.toggle('grayscale');
-    img.addEventListener('click', () => {
-      img.classList.toggle('grayscale');
-    });
-  });
-=======
 function toggleGrayscale(img) {
     img.classList.toggle('grayscale');
     const imageName = img.alt;
     const isGrayscale = img.classList.contains('grayscale');
     localStorage.setItem(imageName, isGrayscale);
->>>>>>> 8afe14aa49c6eca1e32dc142eb5981385a4cbe9e
 }
 
   // Add event listener to each image to toggle grayscale effect
