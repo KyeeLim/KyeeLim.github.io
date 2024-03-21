853b2c0ae2840d906ff7bd1e8c464e0614c88e08
Kyee124 <lim.ky73@gmail.com>
2024-03-21 23:46:27 +0800

    Finally changes

--- js/onClick.js
+++ js/onClick.js
@@ -1,8 +1,9 @@
 // Add event listener to each image to toggle grayscale effect
 document.querySelectorAll('img:not(#banner):not(.timer-table)').forEach(img => {
   //set grayscale to everything
-  img.classList.toggle('grayscale');
+  //img.classList.toggle('grayscale');
   img.addEventListener('click', () => {
-    img.classList.toggle('grayscale');
+    //img.classList.toggle('grayscale');
+	img.style.opacity = img.style.opacity === '0' ? '1' : '0';
   });
 });
