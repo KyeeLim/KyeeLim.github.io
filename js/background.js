e8099480381207215baf9a9a37ad24f05168f10c
AmousuLee <leeamous@gmail.com>
2024-07-17 23:24:12 +0800

    change bg-img to bg-video

---
+++ js/background.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Array of background image URLs
+  var backgroundImages = [
+    'url("../img/background/rickroll2.mp4")',
+    'url("../img/background/rickroll.mp4")'
+    // Add more URLs as needed
+  ];
+
+  // Function to set random background image
+  function setRandomBackground() {
+    var randomNumber = Math.floor(Math.random() * 100);
+    
+    // Check if randomNumber is less than or equal to 5 (which is 6% of 100)
+    if (randomNumber <= 6) {
+      // 6% chance to set the second background image
+      document.querySelector('.background-video').style.backgroundImage = backgroundImages[1];
+      console.log('rickroll2',randomNumber);
+    } else {
+      // 94% chance to set the first background image
+      document.querySelector('.background-video').style.backgroundImage = backgroundImages[0];
+      console.log('rickroll1',randomNumber);
+    }
+  }
+
+  // Initially set a random background image on page load
+  setRandomBackground();
+});
