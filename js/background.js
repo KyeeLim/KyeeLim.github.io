6f3cd2a2e3833120c2b073032e2e2bbd4f74a915
AmousuLee <leeamous@gmail.com>
2024-07-18 15:19:22 +0800

    update background video code

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Array of background image URLs
-  var backgroundImages = [
+  var videos = [
     'url("../img/background/BA_Background.mp4")',
     'url("../img/background/rickroll.mp4")',
     // Add more URLs as needed
@@ -9,19 +9,22 @@ document.addEventListener("DOMContentLoaded", function() {
   // Function to set random background image
   function setRandomBackground() {
     var randomNumber = Math.floor(Math.random() * 100);
+    const videoElement = document.getElementById('backgroundVideo');
     
     // Check if randomNumber is less than or equal to 5 (which is 6% of 100)
     if (randomNumber <= 6) {
       // 6% chance to set the second background image
-      document.querySelector('.background-video').style.backgroundImage = backgroundImages[1];
-      console.log('test',randomNumber);
+      videoElement.innerHTML = `<source src="${videos[1]}" type="video/mp4">`;
+      console.log('rick',randomNumber);
     } else {
       // 94% chance to set the first background image
-      document.querySelector('.background-video').style.backgroundImage = backgroundImages[0];
-      console.log('rickroll',randomNumber);
+      videoElement.innerHTML = `<source src="${videos[0]}" type="video/mp4">`;
+      console.log('roll',randomNumber);
     }
   }
 
   // Initially set a random background image on page load
   setRandomBackground();
 });
+
+
