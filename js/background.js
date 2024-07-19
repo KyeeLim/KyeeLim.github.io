2c4ed0fcddf3e08cba948e4947427d6d12fea564
Kyee124 <lim.ky73@gmail.com>
2024-07-19 22:08:35 +0800

    Update background.js

--- js/background.js
+++ js/background.js
@@ -26,12 +26,12 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     videoElement.load();
-    videoElement.play().catch(error => {
-      console.error('Error attempting to play video:', error);
-      // Handle autoplay policy restrictions
-      videoElement.muted = true;
-      videoElement.play();
-    });
+    //videoElement.play().catch(error => {
+    //  console.error('Error attempting to play video:', error);
+    //  // Handle autoplay policy restrictions
+    //  videoElement.muted = true;
+    //  videoElement.play();
+    //});
 	videoElement.pause();
   }
 
