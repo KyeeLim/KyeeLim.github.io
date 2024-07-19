ca10df34b36b42eb8a253f8ff29a86889ff3a90b
Kyee124 <lim.ky73@gmail.com>
2024-07-19 22:03:06 +0800

    Update hideOverlay.js

--- js/hideOverlay.js
+++ js/hideOverlay.js
@@ -3,6 +3,6 @@
     var overlay = document.getElementById('overlay');
 	var vid = document.getElementById('backgroundVideo');
     overlay.style.display = 'none';
-	vid.pause = false;
-	vid.autoplay = true;
+	vid.autplay = true;
+	vid.muted = false;
   }
